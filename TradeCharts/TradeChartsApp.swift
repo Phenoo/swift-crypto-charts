2cc8b47d49902b68d4f5f16e6d401ea687e7fd7b
Phenoo <descometusah@gmail.com>
2024-08-18 03:38:36 +0100

    Initial Commit

---
+++ TradeCharts/TradeChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeChartsApp.swift
+//  TradeCharts
+//
+//  Created by Eze Chidera Paschal on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TradeChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
